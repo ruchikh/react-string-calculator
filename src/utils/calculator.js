b120722d8c406d6b0d4fa05a43823cd243cc7c6c
ruchikh <ruchikharwar4@gmail.com>
2025-03-05 12:32:18 +0530

    Change in code to pass the test case for negative numbers

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,5 +1,13 @@
 
 export function add(numbers) {
     if (!numbers) return 0;
+
+    const numArray = numbers.split(",").map(Number);
+
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
     return numbers.split(",").map(Number).reduce((sum, num) => sum + num, 0);
 }
