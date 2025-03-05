3b32be4eb30d26a67cda4d7cbfb5b46eee6d7339
ruchikh <ruchikharwar4@gmail.com>
2025-03-05 12:37:44 +0530

    Failing test case for new line

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -21,4 +21,8 @@ test("throws an error for negative numbers", () => {
     expect(() => add("-1,2,-3")).toThrow("Negative numbers not allowed: -1, -3");
 });
 
+test("handles new lines as delimiters", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
+
 
